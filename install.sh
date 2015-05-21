c63146601c835bea7da3bb9ed5817963205f67ab
Serubin <Serubin@serubin.net>
2015-05-21 18:56:09 -0400

    Basic install script - in progress

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Get current dir (so run this script from anywhere)
+
+export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Update dotfiles itself first
+
+[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
+
+# Backing up current configurations
+echo "Moving previous configurations to dotfiles/bak/"
+mkdir $DOTFILES_DIR/bak
+
+if [ -d "$HOME/.bashrc" ]; then
+	mv $HOME/.bashrc $DOTFILES_DIR/bak/.bashrc
+fi
+
+if [ -d "$HOME/.inputrc" ]; then
+	mv $HOME/.inputrc $DOTFILES_DIR/bak/.inputrc
+fi
+
+if [ -d "$HOME/.gitconfig" ]; then
+	mv $HOME/.gitconfig $DOTFILES_DIR/bak/.gitconfig
+fi
+
+if [ -d "$HOME/.gitconfig_global" ]; then
+	mv $HOME/.gitconfig_global $DOTFILES_DIR/bak/.gitconfig_global
+fi
+# Bunch of symlinks
+
+ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
+ln -sfv "$DOTFILES_DIR/runcom/.inputrc" ~
+ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
+ln -sfv "$DOTFILES_DIR/etc/mackup/.mackup.cfg" ~
