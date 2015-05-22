b2e1020adb2198746fa25399e428478c74a2b041
Serubin <Serubin@serubin.net>
2015-05-21 20:09:15 -0400

    Added vim configs to install - no install/vim.sh use yet.

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
 # Get current dir (so run this script from anywhere)
-
 export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Update dotfiles itself first
-
 [ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
 # Backing up current configurations
@@ -35,9 +33,10 @@ fi
 if [ -d "$HOME/.gitconfig_global" ]; then
 	mv $HOME/.vimrc $DOTFILES_DIR/bak/.vimrc
 fi
-# Bunch of symlinks
 
+# Bunch of symlinks
 ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
 ln -sfv "$DOTFILES_DIR/runcom/.inputrc" ~
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 #ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~ # needs creation
+ln -sfv "$DOTFILES_DIR/vim/.vimrc" ~
