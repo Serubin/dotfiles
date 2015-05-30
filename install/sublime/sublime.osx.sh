09c0baf74a32fbf7f8a8de7f53a2e7381754b4f3
Serubin <Serubin@serubin.net>
2015-05-30 18:40:28 -0400

    Added working sublime install for osx

--- install/sublime/sublime.osx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# sublime install for osx
-
-# install sublime
-brew cask install caskroom/versions/sublime-text3
-
-ln -s"`perl -e 'print readlink($ARGV[0]) . "\n"' ~/Applications/Sublime\ Text.app`" "/Applications/Sublime Text.app"
