19ff5dc31debf0dbebfab9cc896e36305b517a69
Serubin <Serubin@serubin.net>
2015-05-30 18:41:10 -0400

    Copies bash_profile

--- install.sh
+++ install.sh
@@ -42,6 +42,7 @@ fi
 echo "Creating symlinks"
 # Bunch of symlinks
 ln -sfv "$DOTFILES_DIR/runcom/.bashrc" ~
+ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
 ln -sfv "$DOTFILES_DIR/runcom/.inputrc" ~
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~ # needs creation
