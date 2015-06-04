2737840db15a2e4beac0f1fe515ccfb15a61946b
Serubin <Serubin@serubin.net>
2015-06-04 15:56:12 -0400

    Moved vim install into the install dir. Added per os scripts. Now auto installs

---
+++ install/vim/vim.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+echo "Backing up previous vim config"
+
+if [ -r "$HOME/.vim" ]; then
+	mv $HOME/.vim $HOME/.dotfiles-bak/
+fi
+
+if [ -r "$HOME/.vimrc" ]; then
+	mv $HOME/.vimrc $HOME/.dotfiles-bak/
+fi
+
+echo "Starting vim install"
+
+# determin distro
+if [ $DISTRO == "Debian" | $DISTRO="Ubuntu" ]; then
+	source $DOTFILES_DIR/install/vim/vim.debian
+elif [ $DISTRO == "Darwin" ]; then
+	source $DOTFILES_DIR/install/vim/vim.osx
+
+# config install
+ln -sfv "$DOTFILES_DIR/vim/.vimrc" ~
+mkdir -p ~/.vim/
+ln -sfv "$DOTFILES_DIR/vim/vundle.vim" ~/.vim/
+
+BUNDLE_DIR=~/.vim/bundle
+
+# Install/update Vundle
+mkdir -p "$BUNDLE_DIR" && (git clone https://github.com/gmarik/vundle.git "$BUNDLE_DIR/vundle" || (cd "$BUNDLE_DIR/vundle" && git pull origin master))
+
+# Install bundles
+vim +PluginInstall +qall
+
+# Compile YouCompleteMe
+cd "$BUNDLE_DIR/YouCompleteMe" && ./install.sh
+
+cd -
+
+# Removing variables
+unset BUNDLE_DIR
