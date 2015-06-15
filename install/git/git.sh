4fdc5c52f652c3f0a7c9aeddb40452e3018233dc
Serubin <Serubin@serubin.net>
2015-06-14 23:56:20 -0400

    Updated git install - osx and debian

---
+++ install/git/git.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo " -- Installing Git"
+
+
+if [ -r "$HOME/.gitconfig" ]; then
+	mv $HOME/.gitconfig $HOME/.dotfiles-bak/
+fi
+
+if [ -r "$HOME/.gitconfig_global" ]; then
+	mv $HOME/.gitconfig_global $HOME/.dotfiles-bak/
+fi
+
+if [ $DISTRO == "Debian" | $DISTRO == "Ubuntu" ]; then
+	source $DOTFILES_DIR/install/git/git.debian
+elif [ $DISTRO = "Darwin" ]; then
+	source $DOTFILES_DIR/install/git/git.osx
+else
+	echo "ERROR: This os doesn't support sublime installations."
+fi
+
+ln -sfv "$DOTFILES_DIR/install/git/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/install/git/.gitignore_global" ~ # needs creation
