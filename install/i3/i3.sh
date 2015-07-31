6b0fad4d9015ba6ae3999ce74ab36c3685cb463d
Serubin <Serubin@serubin.net>
2015-07-31 07:58:07 -0400

    Fixed minor install text error

--- install/i3/i3.sh
+++ install/i3/i3.sh
@@ -3,5 +3,6 @@
 # determin distro
 if [ "$DISTRO" == "Debian" ] || [ "$DISTRO" == "Ubuntu" ]; then
 	source $DOTFILES_DIR/install/i3/i3.debian
+else
 	echo "ERROR: This os doesn't support vim installations."
 fi
