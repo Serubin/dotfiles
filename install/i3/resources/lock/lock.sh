36cbf2ba3a6e95ecf8ef2fb079c3390048fd458b
Serubin <Serubin@serubin.net>
2015-08-03 11:06:33 -0400

    Updated i3 lock scripts to accomidate larger/dual screens

--- install/i3/resources/lock/lock.sh
+++ install/i3/resources/lock/lock.sh
@@ -2,7 +2,7 @@
 # Locking script
 # generously from: https://github.com/jearbear/dotfiles
 
-scrot -e 'convert -resize 20% -fill "#282828" -colorize 50% -blur 0x1 -resize 500% $f ~/.lock/lockbg.png' $HOME/.i3/lock/tmp.png
-convert -gravity center -composite $HOME/.i3/lock/lockbg.png $HOME/.i3/lock/lock.png $HOME/.i3/lock/lockfinal.png
+scrot -e 'convert -resize 20% -fill "#282828" -colorize 50% -blur 0x1 -resize 500% $f ~/.i3/lock/lockbg.png' $HOME/.i3/lock/tmp.png
+convert -gravity southwest -geometry +50+50 -composite $HOME/.i3/lock/lockbg.png $HOME/.i3/lock/lock-txt.png $HOME/.i3/lock/lockfinal.png
 i3lock -u -i $HOME/.i3/lock/lockfinal.png
 rm  $HOME/.i3/lock/lockfinal.png $HOME/.i3/lock/lockbg.png $HOME/.i3/lock/tmp.png
