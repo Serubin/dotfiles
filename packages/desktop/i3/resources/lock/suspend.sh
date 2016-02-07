843c113583ef048a08374650ccadb48d12d47a64
= <=>
2016-02-07 02:29:07 -0500

    Fixed overlooked syntax error

--- packages/desktop/i3/resources/lock/suspend.sh
+++ packages/desktop/i3/resources/lock/suspend.sh
@@ -8,6 +8,6 @@
 
 source ${HOME}/.dotfiles.info
 
-source ${DOTFILES_DIR/packages/desktop/i3/resources/lock/lock.sh
+source ${DOTFILES_DIR}/packages/desktop/i3/resources/lock/lock.sh
 
 sudo pm-suspend
