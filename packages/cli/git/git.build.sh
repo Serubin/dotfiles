a43445ca9a8baff88d20bc6e3ba360e1d0dc0e6a
Serubin <Serubin@serubin.net>
2016-02-10 13:06:39 -0500

    Updated git: Added git.build.sh for build from source. Added fedora, updated debian

---
+++ packages/cli/git/git.build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+########################################
+# Build from source, git
+# Version Control
+########################################
+
+echo "Building git from source"
+
+# Install dependencies
+sudo apt-get install -y curl libcurl4-openssl-dev libexpat1-dev gettext libz-dev libssl-dev build-essential
+
+# Download and compile from source - Ubuntu and Debian repos are annoyingly out of date
+git_version="2.7.1"
+
+cd /tmp
+curl -L --progress https://www.kernel.org/pub/software/scm/git/git-${git_version}.tar.gz | tar xz
+cd git-${git_version}/
+
+./configure
+make prefix=/usr/local all
+
+# Install into /usr/local/bin
+sudo make prefix=/usr/local install
+
+cd ..
+
+echo "Removing temporary files"
+rm -rf git-${git_version}/
+
+cd $PACKAGE_INSTALL
+~
+~
