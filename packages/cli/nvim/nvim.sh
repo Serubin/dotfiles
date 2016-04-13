fe34bee5d223d1a9e1faa6bb16e6ecbc0c8e1af4
Serubin <Serubin@serubin.net>
2016-04-13 14:36:36 -0400

    Cleaned up nvim slightly

--- packages/cli/nvim/nvim.sh
+++ packages/cli/nvim/nvim.sh
@@ -7,27 +7,21 @@
 
 echo "Backing up previous vim config"
 
-if [ -r "${HOME}/.vim" ]; then
-    if [ -r "${HOME}/.dotfiles-bak/.vim" ]; then
-        rm -rf ${HOME}/.dotfiles-bak/.vim 2> /dev/null
+if [ -r "${HOME}/.config/nvim" ]; then
+    if [ -r "${HOME}/.dotfiles-bak/.nvim" ]; then
+        rm -rf ${HOME}/.dotfiles-bak/.nvim 2> /dev/null
     fi
 
 	mv -f ${HOME}/.config/nvim ${HOME}/.dotfiles-bak/ 2> /dev/null
 fi
 
-if [ -r "${HOME}/.config/nvim/init.vim" ]; then
-	mv ${HOME}/.config/nvim/init.vim ${HOME}/.dotfiles-bak/ 2> /dev/null
-fi
-
 # config install
-#ln -sfv "${PACKAGE_INSTALL}/config/.vimrc" ~
+
 mkdir -p ${HOME}/.config/nvim/
 ln -sfv "${PACKAGE_INSTALL}/config/nvim/vundle.vim" ~/.config/nvim/
 ln -sfv "${PACKAGE_INSTALL}/config/nvim/init.vim" ~/.config/nvim/
 
 cp "${PACKAGE_INSTALL}/config/nvim/python.vim" ~/.config/nvim/
-
-cp $PACK
 
 if [ "$DISTRO" == "Arch" ]; then # work around for arch, because smart python linking.
     python2_path=`which python2`
