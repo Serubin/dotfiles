15acea46677dbff434470752d02026c1c017a79f
Serubin <Serubin@Serubin.net>
2016-06-28 13:45:33 -0400

    Now tracks installed packages. WIP

--- install.sh
+++ install.sh
@@ -4,6 +4,56 @@ echo "-------- Setting up Serubin's Dotfiles --------"
 
 source ${HOME}/.dotfiles.info
 
+export flag_help=0
+export flag_update=0
+export flag_list=0
+export flag_install=""
+
+export installed=""
+export installed_len+=0
+
+# process arguments
+set -- $(getopt "huli:" "$@")
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        (-h) flag_help=1                            ;;
+        (-u) flag_update=1                          ;;
+        (-l) flag_list=1                            ;;
+        (-i) flag_install="$flag_install $2"; shift ;;
+        (--) shift; break                           ;;
+        (*)  break                                  ;;
+    esac
+    shift
+done
+
+# Help options
+if [[ ${flag_help} == "1" ]]; then
+    echo "usage: ./install -huli:<install>"
+    echo "    -h                        Usage"
+    echo "    -u                        Update all currently installed packages"
+    echo "    -l                        List all currently installed packages"
+    echo "    -i <package,package2>     Installs given packages"
+
+    exit
+fi
+
+# Update flag
+if [[ ${flag_update} == "1" ]]; then
+    export installed=(${DOTFILES_INSTALLED})
+    export installed_len=${DOTFILES_INSTALLED_LEN}
+fi
+
+# List flag
+if [[ ${flag_list} == "1" ]]; then
+    echo "Not yet implemented"
+    exit
+fi
+# Install flag
+if [[ ${flag_install} != "" ]]; then
+    echo "Not yet implemented"
+    exit
+fi
 # Get current dir (so run this script from anywhere)
 export DOTFILES_DIR="$( \cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -57,31 +107,43 @@ fi
 ln -sfv "${DOTFILES_DIR}/common/dircolors-solarized/dircolors.256dark" ~/.dir_colors
 
 # package installations
-registerPackage "cli" "required" # required packages
+if [[ ${flag_update} != "1" ]]; then # if update, don't prompt
+    registerPackage "cli" "required" # required packages
 
-echo "Which shell would you like to use? It's recommend to select ONE."
-registerPackage "shell" "bash"
-registerPackage "shell" "zsh"
+    echo "Which shell would you like to use? It's recommend to select ONE."
+    registerPackage "shell" "bash"
+    registerPackage "shell" "zsh"
 
-registerPackage "cli" "git"
-registerPackage "cli" "vim"
-registerPackage "cli" "nvim"
-registerPackage "cli" "tmux"
-registerPackage "cli" "htop"
-registerPackage "cli" "archey"
-registerPackage "cli" "vhdl"
+    registerPackage "cli" "git"
+    registerPackage "cli" "vim"
+    registerPackage "cli" "nvim"
+    registerPackage "cli" "tmux"
+    registerPackage "cli" "htop"
+    registerPackage "cli" "archey"
+    registerPackage "cli" "vhdl"
 
-# Prompt for desktop
-if [[ `getInputBoolean "Would you like to install desktop packages?"` == "1" ]]; then
-	registerPackage "desktop" "sublime"
-	registerPackage "desktop" "i3"
-    registerPackage "desktop" "latex"
+    # Prompt for desktop
+    if [[ `getInputBoolean "Would you like to install desktop packages?"` == "1" ]]; then
+        registerPackage "desktop" "sublime"
+        registerPackage "desktop" "i3"
+        registerPackage "desktop" "latex"
+    fi
+    echo 's/DOTFILES_INSTALLED=".*"/DOTFILES_INSTALLED="'${installed[@]}'"/g' 
+    echo $installed_len
+    sed -i -e 's/DOTFILES_INSTALLED=".*"/DOTFILES_INSTALLED="'${installed[@]}'"/g' ${HOME}/.dotfiles.info
+    sed -i -e 's/DOTFILES_INSTALLED_LEN=".*"/DOTFILES_INSTALLED_LEN="'${installed_len}'"/g' ${HOME}/.dotfiles.info
+
+    echo "------------ Installing "
+    installPackage "" ""
 fi
 
-echo "------------ Installing "
-installPackage
+# package updates
+if [[ ${flag_update} == "1" ]]; then # if update, don't prompt
+    echo "------------ Updating "
 
+    installPackage ${installed_len} ${installed[@]}
+fi
 cd $DOTFILES_DIR
 
 # Removing variables
-unset DOTFILES_DIR
+unset flag_update flat_install DOTFILES_DIR
