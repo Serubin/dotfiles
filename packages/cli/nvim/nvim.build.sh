bb6d0efce2d2fc9f24213eae6a2382771cb04bed
Serubin <Serubin@serubin.net>
2017-02-09 11:22:43 -0500

    Fixed nvim build. Changed htop

--- packages/cli/nvim/nvim.build.sh
+++ packages/cli/nvim/nvim.build.sh
@@ -4,6 +4,8 @@
 # A better Vi Improved
 ########################################
 
+sudo apt-get -y install build-essential libtool libtool-bin autoconf automake cmake g++ pkg-config unzip python-dev python3-dev xsel 
+
 cd /tmp # Move to tmp
 git clone https://github.com/neovim/neovim.git neovim
 cd neovim
