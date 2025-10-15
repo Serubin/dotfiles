baaec04c0d62af9aa043d9435f0a8cce302af6c1
Serubin <Serubin@serubin.net>
2025-10-15 13:28:51 -0400

    chore(install): Fix install script

--- install.sh
+++ install.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
+dotfilesDir=$(pwd)
 source ./zsh/.zsh/01-os
 
-bash ./setup/$DISTRO
+sudo bash ./setup/$DISTRO
 
 stowOptions="-t $HOME --ignore setup -R"
 
-tools=("git", "zsh")
+tools=("git" "zsh")
 
 for tool in "${tools[@]}"; do
     echo " ==== Setting up $tool ==== "
     cd $tool
-    bash setup/common
-    bash setup/$DISTRO
-    cd -
+
+    sudo bash setup/common
+    sudo bash setup/$DISTRO
+    cd $dotfilesDir
+
     echo "stow $tool $stowOptions"
     stow $tool $stowOptions
 done
