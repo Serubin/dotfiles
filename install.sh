610d97eaf230abde5afc1c143aea0caae6dea505
Serubin <Serubin@serubin.net>
2025-10-16 00:10:47 -0400

    feat(tmux): Add tmux

--- install.sh
+++ install.sh
@@ -1,22 +1,44 @@
 #!/bin/bash
 
+## Script options
+stowOptions="-t $HOME --ignore setup -R -v"
+tools=("git" "zsh" "tmux")
+
+## Check for flags
+# Check for '-v' flag
+verbose=false
+for arg in "$@"; do
+    if [ "$arg" == "-v" ]; then
+        verbose=true
+        break
+    fi
+done
+
+redirect=/dev/stdout
+[ "$verbose" = false ] && redirect=/dev/null
+
+## Install logic
 dotfilesDir=$(pwd)
+_HOME=$HOME
 source ./zsh/.zsh/01-os
 
-sudo bash ./setup/$DISTRO
-
-stowOptions="-t $HOME --ignore setup -R"
-
-tools=("git" "zsh")
+echo "Setting up $DISTRO..."
+sudo HOME=$HOME bash ./setup/$DISTRO > $redirect 2>&1
 
 for tool in "${tools[@]}"; do
     echo " ==== Setting up $tool ==== "
+    echo -n "Stowing $tool... "
+    stow $tool $stowOptions
+
     cd $tool
 
-    sudo bash setup/common
-    sudo bash setup/$DISTRO
+
+    echo -n "Running install for $tool... "
+    [ -f "setup/$DISTRO" ] && sudo HOME=$HOME bash setup/$DISTRO > $redirect 2>&1
+    [ -f "setup/common" ] && sudo HOME=$HOME bash setup/common > $redirect 2>&1
     cd $dotfilesDir
 
-    echo "stow $tool $stowOptions"
-    stow $tool $stowOptions
+    echo "Done"
 done
+
+echo "All tools installed"
