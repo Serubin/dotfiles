f6cd78124cf56caecb589d2237a97b74a8d959d5
Serubin <Serubin@serubin.net>
2025-11-15 21:52:01 -0500

    chore(): Misc setup script cleanups

--- install.sh
+++ install.sh
@@ -9,10 +9,10 @@ configTools=("nvim")
 # Check for '-v' flag
 verbose=false
 for arg in "$@"; do
-    if [ "$arg" == "-v" ]; then
-        verbose=true
-        break
-    fi
+  if [ "$arg" == "-v" ]; then
+    verbose=true
+    break
+  fi
 done
 
 redirect=/dev/stdout
@@ -24,26 +24,25 @@ _HOME=$HOME
 source ./zsh/.zsh/01-os
 
 echo "Setting up $DISTRO..."
-sudo HOME=$HOME bash ./setup/$DISTRO > $redirect 2>&1
+sudo HOME=$HOME bash ./setup/$DISTRO >$redirect 2>&1
 
 for tool in "${tools[@]}"; do
-    echo " ==== Setting up $tool ==== "
-    echo -n "Stowing $tool... "
+  echo " ==== Setting up $tool ==== "
+  echo -n "Stowing $tool... "
 
-    if [[ " ${configTools[@]} " =~ " $tool " ]]; then
-        configOption="-t $HOME/.config/"
-    fi
-    stow $tool $stowOptions $configOption
+  if [[ " ${configTools[@]} " =~ " $tool " ]]; then
+    configOption="-t $HOME/.config/"
+  fi
+  stow $tool $stowOptions $configOption
 
-    cd $tool
+  cd $tool
 
+  echo -n "Running install for $tool... "
+  [ -f "setup/$DISTRO" ] && bash setup/$DISTRO >$redirect 2>&1
+  [ -f "setup/common" ] && bash setup/common >$redirect 2>&1
+  cd $dotfilesDir
 
-    echo -n "Running install for $tool... "
-    [ -f "setup/$DISTRO" ] && sudo HOME=$HOME bash setup/$DISTRO > $redirect 2>&1
-    [ -f "setup/common" ] && sudo HOME=$HOME bash setup/common > $redirect 2>&1
-    cd $dotfilesDir
-
-    echo "Done"
+  echo "Done"
 done
 
 echo "All tools installed"
