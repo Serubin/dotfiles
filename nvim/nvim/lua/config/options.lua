b93a582fb0a50bd91fc66c33b2c089b892325fb3
Solomon <serubin@serubin.net>
2025-11-17 12:02:45 -0500

    bugfix(nvim): Small nvim fixes

--- nvim/nvim/lua/config/options.lua
+++ nvim/nvim/lua/config/options.lua
@@ -2,5 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.autoformat = false
 vim.opt.relativenumber = false
 vim.opt.scrolloff = 8
+vim.opt.fixendofline = true
+
--- nvim/nvim/lua/plugins/betterwhitespace.lua
+++ nvim/nvim/lua/plugins/betterwhitespace.lua
@@ -1,10 +1,21 @@
+local disabled_filetypes = {
+  "snacks_dashboard",
+  "snacks_picker_list",
+  "lazy",
+}
+
 return {
-  'ntpeters/vim-better-whitespace',
+  "ntpeters/vim-better-whitespace",
   lazy = false,
   event = "BufReadPost", -- Or BufNewFile
-  opts = {
-    -- Options to customize behavior if needed
-  },
+  enabled = function()
+    local disabled = false
+    local node = vim.treesitter.get_node()
+    disabled = disabled or (vim.tbl_contains(disabled_filetypes, vim.bo.filetype))
+    disabled = disabled or (vim.bo.buftype == "prompt")
+    disabled = disabled or not not (node and string.find(node:type(), "comment"))
+    return not disabled
+  end,
   config = function(_, opts)
     -- Optional: Add custom highlighting for specific modes/filetypes
     vim.api.nvim_set_hl(0, "ExtraWhitespace", { bg = "red" })
@@ -12,5 +23,5 @@ return {
     -- Enable highlighting
     vim.g.better_whitespace_enabled = true
     vim.g.strip_whitespace_on_save = true -- Auto-remove on save
-  end
+  end,
 }
